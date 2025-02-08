9c4773a8daf14e46b1d78c3081813e9580bf6c2f
Yota0525 <KTC24A31G0013@edu.kyoto-tech.ac.jp>
2025-02-08 10:12:31 +0900

    ページ内容作成

--- pages/singers.vue
+++ pages/singers.vue
@@ -2,9 +2,27 @@
   <div class="singers-page">
     <h1>Singer Introductions</h1>
     <ul>
-      <li>Artist A - Genre</li>
-      <li>Artist B - Genre</li>
-      <li>Artist C - Genre</li>
+      <li>秋川雅史 - 代表曲「千の風になって」</li>
+      <li>安室奈美恵 - 代表曲「Can You Celebrate」</li>
+      <li>絢香 - 代表曲「三日月」</li>
+      <li>嵐 - 代表曲「A・RA・SHI」</li>
+      <li>アンジェラ・アキ - 代表曲「手紙〜拝啓 十五の君へ〜」</li>
+      <li>家入レオ - 代表曲「君がくれた夏」</li>
+      <li>いきものがかり - 代表曲「YELL」</li>
+      <li>石井竜也 - 代表曲「浪漫飛行」</li>
+      <li>大黒摩季 - 代表曲「ら・ら・ら」</li>
+      <li>大塚愛 - 代表曲「さくらんぼ」</li>
+      <li>コブクロ - 代表曲「蕾」</li>
+      <li>倖田來未 - 代表曲「愛のうた」</li>
+      <li>倉木麻衣 - 代表曲「Secret of my heart」</li>
+      <li>桑田佳祐 - 代表曲「明日晴れるかな」</li>
+      <li>AI - 代表曲「ハピネス」</li>
+      <li>Aiko - 代表曲「カブトムシ」</li>
+      <li>AKB48 - 代表曲「ヘビーローテーション」</li>
+      <li>Aqua Timez - 代表曲「虹」</li>
+      <li>Every Little Thing - 代表曲「Time goes by」</li>
+      <li>EXILE - 代表曲「Choo Choo Train」</li>
+      <li>THE ALFEE - 代表曲「星空の下のディスタンス」</li>
     </ul>
   </div>
 </template>
--- pages/songs.vue
+++ pages/songs.vue
@@ -2,9 +2,26 @@
   <div class="songs-page">
     <h1>Song Introductions</h1>
     <ul>
-      <li>Song 1 - Artist A</li>
-      <li>Song 2 - Artist B</li>
-      <li>Song 3 - Artist C</li>
+      <li>アイドル - アニメ「推しの子」主題歌</li>
+      <li>奏 - 映画「ラフ ROUGH」挿入歌</li>
+      <li>キセキ - ドラマ「ROOKIES」主題歌</li>
+      <li>クリスマスソング - ドラマ「5→9～私に恋したお坊さん～」主題歌</li>
+      <li>紅蓮華 - アニメ「鬼滅の刃」主題歌</li>
+      <li>前前前世 - 映画「君の名は」主題歌</li>
+      <li>高嶺の花子さん - ドラマ「海のはじまり」主題歌</li>
+      <li>小さな恋の歌 - オリックスバファローズ「宮城大弥投手」登場曲</li>
+      <li>トリセツ - 映画「ヒロイン失格」主題歌</li>
+      <li>猫 - ドラマ25「猫」主題歌</li>
+      <li>白日 - ドラマ「イノセンス 冤罪弁護士」主題歌</li>
+      <li>ハッピーエンド - 映画「ぼくは明日、昨日のきみとデートする」主題歌</li>
+      <li>花束 - ドラマ「オトナ女子」主題歌</li>
+      <li>マリーゴールド - 麒麟麦酒「淡麗グリーンラベル」CM曲</li>
+      <li>Bling-Bang-Bang-Born - アニメ「マッシュル-MASHLE-」主題歌</li>
+      <li>Lemon - ドラマ「アンナチュラル」主題歌</li>
+      <li>Loser - Honda「Jade」CM曲</li>
+      <li>Pretender - 映画「コンフィデンスマンJP ロマンス編」主題歌</li>
+      <li>RPG - 映画「クレヨンしんちゃん バカうまっ！ B級グルメサバイバル」主題歌</li>
+      <li>366日 - 映画「赤い糸」主題歌</li>
     </ul>
   </div>
 </template>
