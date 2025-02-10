95af9024ff563d0bfcaa423b2c698ac2b34d4e52
Yota0525 <KTC24A31G0013@edu.kyoto-tech.ac.jp>
2025-02-10 10:50:36 +0900

    内容作成

--- pages/products.vue
+++ pages/products.vue
@@ -2,9 +2,27 @@
   <div class="products-page">
     <h1>Music Product Introductions</h1>
     <ul>
-      <li>Product 1 - Description</li>
-      <li>Product 2 - Description</li>
-      <li>Product 3 - Description</li>
+      <li>Arius - 本格的な音とタッチ、表現力で新しいピアノ体験を実現します</li>
+      <li>AvantGrand - 自然な振動までも再現し、演奏者とピアノの一体感を作り出します</li>
+      <li>Celesta - フェルト巻きのハンマーにより、共鳴箱付きの金属音板を叩いて高音域を発生させる楽器</li>
+      <li>Clavinova - 本格的な演奏性を実現するだけでなく、デジタルならではの利便性や楽しみも提供します</li>
+      <li>Disklavier - 自動演奏ピアノを越えたエンターテイメントピアノ</li>
+      <li>Electone - オールマイティな用途に対応する電子オルガン</li>
+      <li>Flute - 木管楽器の一種で、リードを使わないエアリード式の横笛</li>
+      <li>Grand Piano - ピアノ全体が調和し、弾く程に多彩な音色と透き通った響きに成長します</li>
+      <li>Harmony Director - 吹奏楽や合唱、オーケストラなどの指導に活用されている音楽指導用のキーボード</li>
+      <li>Clarinet - 1枚の簧を振動源として音を出す木管楽器</li>
+      <li>P Series - 弾きたい場所で気の向くままに本格的なピアノの響きをお楽しみいただけます</li>
+      <li>Piaggero - 軽量・コンパクトで、美しいピアノ音と自然なタッチの鍵盤を搭載</li>
+      <li>Pìccolo - 世界から高い評価を受ける確かな音程と深みのある音色。</li>
+      <li>Pianica - 2オクターブ半の音域を持ち、ひときわ澄んだ音色と美しい集合音で、音量も豊か</li>
+      <li>Portable Keyboard - 高品質なサウンドと多彩な機能、タッチレスポンス付き鍵盤搭載</li>
+      <li>Silent Piano - 進化した消音機能と豊かな音の響きで、いつでも、好きなときに向き合えます</li>
+      <li>Sonogenic - アクティブな演奏を楽しんだり、充実のスペックで音楽制作も可能な、エンターテインメント性に優れた新しいタイプの楽器</li>
+      <li>Stage Piano - ライブ演奏に最適なピアノ</li>
+      <li>Synthesizer - 主に電子工学的手法により楽音等を合成する楽器</li>
+      <li>TransAcoustic Piano - 豊かな響きを感じながら、消音・音量調節ができます</li>
+      <li>Upright Piano - 豊かな音と確かな品質であなたの想いや情熱に、しっかりと応えてくれます</li>
     </ul>
   </div>
 </template>
