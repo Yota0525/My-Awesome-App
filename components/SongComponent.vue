f91bd1d6392b748b66bdc986667d1188918f08b7
Yota0525 <167695062+Yota0525@users.noreply.github.com>
2025-03-05 03:20:00 +0000

    補習かだい

---
+++ components/SongComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <img :src="require(`~/assets/image/${image}`)" alt="songs" class="songs-image" :style="{ width: 'auto', height: 'auto', objectFit: 'cover', borderRadius: 'auto', marginRight: 'auto' }" />
+        <div class="songs-info" :style="{ display: 'flex', flexDirection: 'column' }">
+          <span class="songs-name" :style="{ fontSize: '55px', fontWeight: 'bold' }">{{ name }}</span>
+          <span class="songs-details" :style="{ fontSize: '35px', color: '#888' }">{{ details }}</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    name : {
+        type : String,
+        required : true,
+    },
+    details : {
+        type : String,
+        reqired : true,
+    },
+    image : {
+        type : String,
+        required : true,
+    },
+});
+
+</script>
---
+++ composables/useSongs.ts
@@ -0,0 +1,26 @@
+export default function useSongs() {
+    const songs = [
+        { name: "アイドル", details: "アニメ「推しの子」主題歌", image: "Idol.jpeg" },
+        { name: "奏", details: "映画「ラフ ROUGH」挿入歌", image: "Kanade.jpeg" },
+        { name: "キセキ", details: "ドラマ「ROOKIES」主題歌", image: "Rookies.jpeg" },
+        { name: "クリスマスソング", details: "ドラマ「5→9～私に恋したお坊さん～」主題歌", image: "5→9.jpeg" },
+        { name: "紅蓮華", details: "アニメ「鬼滅の刃」主題歌", image: "Kimetsu.jpeg" },
+        { name: "前前前世", details: "映画「君の名は」主題歌", image: "Kimi.jpeg" },
+        { name: "高嶺の花子さん", details: "ドラマ「海のはじまり」主題歌", image: "Umi.jpeg" },
+        { name: "小さな恋の歌", details: "オリックスバファローズ「宮城大弥投手」登場曲", image: "Orix.jpeg" },
+        { name: "トリセツ", details: "映画「ヒロイン失格」主題歌", image: "Hiroin.jpeg" },
+        { name: "猫", details: "ドラマ25「猫」主題歌", image: "Neko.jpeg" },
+        { name: "白日", details: "ドラマ「イノセンス 冤罪弁護士」主題歌", image: "Inosennsu.jpeg" },
+        { name: "ハッピーエンド", details: "映画「ぼくは明日、昨日のきみとデートする」主題歌", image: "Boku.jpeg" },
+        { name: "花束", details: "ドラマ「オトナ女子」主題歌", image: "Otona.jpeg" },
+        { name: "マリーゴールド", details: "麒麟麦酒「淡麗グリーンラベル」CM曲", image: "Green.jpeg" },
+        { name: "Bling-Bang-Bang-Born", details: "アニメ「マッシュル-MASHLE-」主題歌", image: "Mashle.jpeg" },
+        { name: "Lemon", details: "ドラマ「アンナチュラル」主題歌", image: "An.jpeg" },
+        { name: "Loser", details: "Honda「Jade」CM曲", image: "Jade.jpeg" },
+        { name: "Pretender", details: "映画「コンフィデンスマンJP ロマンス編」主題歌", image: "Romannsu.jpeg" },
+        { name: "RPG", details: "映画「クレヨンしんちゃん バカうまっ！ B級グルメサバイバル」主題歌", image: "Bkyuu.jpeg" },
+        { name: "366日", details: "映画「赤い糸」主題歌", image: "Red.jpeg" },
+    ];
+
+    return { songs };
+}
