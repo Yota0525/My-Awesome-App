0de4d0c662fa27dd3a09166724432835eabc9913
Yota0525 <167695062+Yota0525@users.noreply.github.com>
2025-03-05 03:42:34 +0000

    fg

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,15 @@
 export default {
-    css: [
-      '@/assets/styles/main.css',  // グローバルスタイルを追加
+  css: [
+    '@/assets/styles/main.css',  // グローバルスタイルを追加
+  ],
+
+  head: {
+    title: 'Blue Music World',  // タイトルを設定
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     ],
-    head: {
-      title: 'Blue Music World',  // タイトルを設定
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      ],
-    },
-  }
-  
+  },
+
+  compatibilityDate: '2025-03-05'
+};
